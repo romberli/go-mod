f734b215d09d8fccd6fc15d0831a13c2ee221664
Romber Li <romber2001@163.com>
2024-09-05 17:34:00 +0800

    Initial commit

---
+++ config/config.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2020 Romber Li <romber2001@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/romberli/go-util/constant"
+	"github.com/romberli/log"
+	"github.com/spf13/viper"
+)
+
+var (
+	ValidLogLevels  = []string{"debug", "info", "warn", "warning", "error", "fatal"}
+	ValidLogFormats = []string{"text", "json"}
+)
+
+// SetDefaultConfig set default configuration, it is the lowest priority
+func SetDefaultConfig(baseDir string) {
+	// daemon
+	SetDefaultDaemon()
+	// log
+	SetDefaultLog(baseDir)
+	// server
+	SetDefaultServer(baseDir)
+
+	// daemon
+	viper.SetDefault(DaemonKey, DefaultDaemon)
+	// log
+	defaultLogFile := filepath.Join(baseDir, DefaultLogDir, log.DefaultLogFileName)
+	viper.SetDefault(LogFileNameKey, defaultLogFile)
+	viper.SetDefault(LogLevelKey, log.DefaultLogLevel)
+	viper.SetDefault(LogFormatKey, log.DefaultLogFormat)
+	viper.SetDefault(LogMaxSizeKey, log.DefaultLogMaxSize)
+	viper.SetDefault(LogMaxDaysKey, log.DefaultLogMaxDays)
+	viper.SetDefault(LogMaxBackupsKey, log.DefaultLogMaxBackups)
+	// server
+	viper.SetDefault(ServerAddrKey, DefaultServerAddr)
+	defaultPidFile := filepath.Join(baseDir, fmt.Sprintf("%s.pid", DefaultCommandName))
+	viper.SetDefault(ServerPidFileKey, defaultPidFile)
+	viper.SetDefault(ServerReadTimeoutKey, DefaultServerReadTimeout)
+	viper.SetDefault(ServerWriteTimeoutKey, DefaultServerWriteTimeout)
+}
+
+// SetDefaultDaemon sets the default value of daemon
+func SetDefaultDaemon() {
+	viper.SetDefault(DaemonKey, DefaultDaemon)
+}
+
+// SetDefaultLog sets the default value of log
+func SetDefaultLog(baseDir string) {
+	defaultLogFile := filepath.Join(baseDir, DefaultLogDir, log.DefaultLogFileName)
+	viper.SetDefault(LogFileNameKey, defaultLogFile)
+	viper.SetDefault(LogLevelKey, log.DefaultLogLevel)
+	viper.SetDefault(LogFormatKey, log.DefaultLogFormat)
+	viper.SetDefault(LogMaxSizeKey, log.DefaultLogMaxSize)
+	viper.SetDefault(LogMaxDaysKey, log.DefaultLogMaxDays)
+	viper.SetDefault(LogMaxBackupsKey, log.DefaultLogMaxBackups)
+	viper.SetDefault(LogRotateOnStartupKey, DefaultRotateOnStartup)
+	viper.SetDefault(LogStdoutKey, DefaultLogStdout)
+}
+
+// SetDefaultServer sets the default value of server
+func SetDefaultServer(baseDir string) {
+	viper.SetDefault(ServerAddrKey, DefaultServerAddr)
+	defaultPidFile := filepath.Join(baseDir, fmt.Sprintf("%s.pid", DefaultCommandName))
+	viper.SetDefault(ServerPidFileKey, defaultPidFile)
+	viper.SetDefault(ServerReadTimeoutKey, DefaultServerReadTimeout)
+	viper.SetDefault(ServerWriteTimeoutKey, DefaultServerWriteTimeout)
+}
+
+// TrimSpaceOfArg trims spaces of given argument
+func TrimSpaceOfArg(arg string) string {
+	args := strings.SplitN(arg, constant.EqualString, 2)
+
+	switch len(args) {
+	case 1:
+		return strings.TrimSpace(args[0])
+	case 2:
+		argName := strings.TrimSpace(args[0])
+		argValue := strings.TrimSpace(args[1])
+		return fmt.Sprintf("%s=%s", argName, argValue)
+	default:
+		return arg
+	}
+}
---
+++ go.mod
@@ -0,0 +1,55 @@
+module github.com/romberli/go-template-cli
+
+go 1.19
+
+replace github.com/spf13/pflag v1.0.5 => github.com/romberli/pflag v1.0.6-alpha
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/pingcap/errors v0.11.5-0.20211224045212-9687c2b0f87c
+	github.com/romberli/go-multierror v1.1.2
+	github.com/romberli/go-util v0.3.18-0.20230824104336-dd4cc54fb877
+	github.com/romberli/log v1.0.27
+	github.com/spf13/cast v1.5.1
+	github.com/spf13/cobra v1.7.0
+	github.com/spf13/viper v1.16.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/sftp v1.13.6 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/romberli/dynamic-struct v1.2.1 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.7 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.25.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apimachinery v0.28.0 // indirect
+)
